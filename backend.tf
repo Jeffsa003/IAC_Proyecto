3ab451572361be55eba9598abcfe8ad5230fe38f
jbrays <bryan20santillan@gmail.com>
2025-07-03 20:55:54 -0500

    ajuste de pipeline apply2

---
+++ backend.tf
@@ -0,0 +1,27 @@
+# backend.tf
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${lower(var.project_name)}-terraform-state-${data.aws_caller_identity.current.account_id}"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "${var.project_name}-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "elearningproject-terraform-state-471112539322" # Reemplazar con el nombre del bucket creado
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "elearningproject-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
